0a0fa83aebd57abedc69ea4b9cabca0cf4172169
Mehdi Bellam <porcepic91@gmail.com>
2025-04-15 10:14:19 +0200

    création des routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import {routes  } from '../src/routes.js';
 
-createApp(App).mount('#app')
+// Configuration du routeur
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+// Création de l'application Vue
+const app = createApp(App);
+
+// Utilisation du routeur
+app.use(router);
+
+// Montage de l'application
+app.mount('#app');
