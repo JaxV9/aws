0587a3c7923d6fb72358f0640986d70d097494f2
JaxV9 <layanj.pro@gmail.com>
2025-04-15 16:53:31 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from './routes';
 import App from './App.vue';
+import { Amplify } from 'aws-amplify';
+import awsmobile from './aws-exports';
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+Amplify.configure(awsmobile)
 
 createApp(App).use(router).mount('#app');
