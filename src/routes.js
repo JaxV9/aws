9b06a43c25eebffdffc445423bba506045f03321
JaxV9 <layanj.pro@gmail.com>
2025-04-15 21:41:10 +0200

    fix: route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-import App from "./App.vue";
 import RegisterForm from "./views/RegisterForm.vue";
 import UserPage from "./components/UserPage.vue";
 import Login from "./views/Login.vue";
 import ConfirmSignUp from "./views/ConfirmSignUp.vue";
+import HomePage from "./views/HomePage.vue";
 
 export const routes = [
-    { path: "/", component: App },
+    { path: "/", component: HomePage },
     { path: "/register", component: RegisterForm },
     { path: "/login", component: Login},
     { path: "/user", component: UserPage },
---
+++ src/views/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+    <DarkModeLayout>
+        <section>
+            <h1>Home</h1>
+        </section>
+    </DarkModeLayout>
+</template>
+
+<script>
+import DarkModeLayout from '@/layouts/DarkModeLayout.vue';
+
+
+export default {
+    name: "HomePage",
+    components: {
+        DarkModeLayout,
+    },
+};
+</script>
+
+<style>
+section {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 150px;
+}
+
+h1 {
+    color: white;
+}
+</style>
