5759f4cbe050a29541830bfa81a0df40b959185c
Felix/failaxite <failaxite.don@gmail.com>
2025-04-15 14:16:00 +0200

    send bugs for jason

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { Amplify } from 'aws-amplify';
+import awsmobile from './aws-exports';
+
+Amplify.configure(awsmobile);
+
 createApp(App).use(router).mount('#app');
