1270f40e5e5c42bec5c0fbd0543c4526aff2483b
JaxV9 <layanj.pro@gmail.com>
2025-04-15 14:07:41 +0200

    update trigger

--- amplify/backend/function/devops276efd32PostConfirmation/src/custom.js
+++ amplify/backend/function/devops276efd32PostConfirmation/src/custom.js
@@ -4,16 +4,19 @@
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
 const lambdaClient = new LambdaClient()
 
+
 exports.handler = async (event, context) => {
   // insert code to be executed by your lambda trigger
   if(event.triggerSource === "PostConfirmation_ConfirmSignUp") {
     const user_id = event.userName;
     const email = event.request.userAttributes.email;
+    const first_name = event.request.userAttributes.name;
+    const last_Name = event.request.userAttributes.family_name
 
     const asyncLambdaParams = {
-      FunctionName: process.env.STORAGE_USERS_NAME,
+      FunctionName: process.env.FUNCTION_CREATEUSER_NAME,
       InvocationType: "Event",
-      Payload: JSON.stringify({user_id, email})
+      Payload: JSON.stringify({user_id, email, first_name, last_Name})
     }
     const command = new InvokeCommand(asyncLambdaParams)
     await lambdaClient.send(command)
